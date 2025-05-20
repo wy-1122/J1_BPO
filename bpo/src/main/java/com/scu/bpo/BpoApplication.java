c52357f4b214621f785852926ed985243f8b6309
EIPsilly <1020341547@qq.com>
2025-05-20 17:10:09 +0800

    新建项目

---
+++ bpo/src/main/java/com/scu/bpo/BpoApplication.java
@@ -0,0 +1,13 @@
+package com.scu.bpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpoApplication.class, args);
+	}
+
+}
---
+++ bpo/src/test/java/com/scu/bpo/BpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scu.bpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
