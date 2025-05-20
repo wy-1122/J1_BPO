120b7da842cb2df61eebe28925739ecff46c6bd7
EIPsilly <1020341547@qq.com>
2025-05-20 17:06:42 +0800

    删除原intellij项目

--- bpo_2/src/main/java/com/database/bpo/service/ClientSupportService.java
+++
@@ -1,8 +0,0 @@
-package com.database.bpo.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ClientSupportService {
-    public String getEquipmentName(Integer equipmentId);
-}
--- bpo_2/src/main/java/com/database/bpo/service/ProjectTypeService.java
+++
@@ -1,8 +0,0 @@
-package com.database.bpo.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ProjectTypeService {
-    public String findProjectTypeName(Integer projectTypeId);
-}
